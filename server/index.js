5db78331ba9cfc3b682359366b4961fa3da3943d
gourav-1711 <gouravdadhich34@gmail.com>
2026-01-21 20:03:33 +0530

    cors fix

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.use(
 app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
-    origin:[ "http://localhost:3000" , "https://realtime-chat-app-jxhf.onrender.com"],
+    origin:[ "http://localhost:3000" , "https://realtime-chat-app-delta-coral.vercel.app"],
     credentials: true,
   }),
 );
